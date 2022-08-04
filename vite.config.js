b67facb834b0175925910eb7d9ab244fe89c39e9
orangeguseul <orangeguseul@gmail.com>
2022-08-04 21:42:42 +0900

    set index.html

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: './index.html', // default
+      },
+    },
+  },
 })
