6ea2241f8c78f078d3ad4ba931a20dd9283e2812
orangeguseul <orangeguseul@gmail.com>
2022-08-04 11:39:58 +0900

    initial github

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  // @ts-ignore
+import Tailwindcss from "./Tailwindcss.svelte";
+let keyword
+let FormEntered = (event)=>{
+  event.preventDefault()
+  location.href = `https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=${keyword}`
+}
+
+</script>
+<Tailwindcss/>
+<main>
+  <div>
+    <form action="get" class="grid" on:submit={FormEntered}>
+      <input type="text" bind:value = {keyword} class="bg-white justify-self-center rounded-lg w-96 h-10 mt-56 font-sans focus: outline-none caret-slate-400 hover:border-lime-400 border-2">
+      <input type="submit" class="invisible">
+    </form>
+  </div>
+</main>
+
+<style>
+  :global(body){
+    background-color: #34d399;
+  }
+</style>
